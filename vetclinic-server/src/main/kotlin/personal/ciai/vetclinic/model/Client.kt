a91962dc9ecfffe9adb9f30f2bf84e55cdc07726
Tomas Pessanha <t.pessanha@campus.fct.unl.pt>
2019-10-04 00:26:12 +0100

    Added Client properties

--- vetclinic-server/src/main/kotlin/personal/ciai/vetclinic/model/Client.kt
+++ vetclinic-server/src/main/kotlin/personal/ciai/vetclinic/model/Client.kt
@@ -1,6 +1,8 @@
 package personal.ciai.vetclinic.model
 
-class Client<ClientDTO>(id: Int) : User<ClientDTO>(id) {
+class Client<ClientDTO>(id: Int, name: String, username: String, password: String) : User<ClientDTO>(id = id, name, username,
+    password
+) {
     override fun toDTO(): ClientDTO {
         TODO("not implemented")
     }
