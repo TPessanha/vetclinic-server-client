fa61adf655bdf38675df0066271b37f7d2055c56
Tomas Pessanha <t.pessanha@campus.fct.unl.pt>
2019-10-03 01:14:05 +0100

    Fixed Ktlint errors

--- vetclinic-server/src/main/kotlin/personal/ciai/vetclinic/model/Entity.kt
+++ vetclinic-server/src/main/kotlin/personal/ciai/vetclinic/model/Entity.kt
@@ -2,6 +2,6 @@ package personal.ciai.vetclinic.model
 
 import java.io.Serializable
 
-abstract class Entity<out DTO>(val id: Int) : Serializable{
+abstract class Entity<out DTO>(val id: Int) : Serializable {
     abstract fun toDTO(): DTO
 }
