210a5f6dcf8f1226e0bd729fa6720f6557f3971a
Tomas Pessanha <t.pessanha@campus.fct.unl.pt>
2019-10-12 00:34:36 +0100

    Fixed pet Integration test

--- vetclinic-server/src/test/kotlin/personal/ciai/vetclinic/IntegrationTests/PetTests.kt
+++ vetclinic-server/src/test/kotlin/personal/ciai/vetclinic/IntegrationTests/PetTests.kt
@@ -2,23 +2,28 @@ package personal.ciai.vetclinic.IntegrationTests
 
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Assertions.assertNotEquals
+import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.context.junit.jupiter.SpringExtension
+import org.springframework.transaction.annotation.Transactional
 import personal.ciai.vetclinic.ExampleObjects.exampleObjects.dogExample
 import personal.ciai.vetclinic.service.PetService
 
 @ExtendWith(SpringExtension::class)
 @SpringBootTest
+@Transactional
 class PetTests {
     @Autowired
     lateinit var petService: PetService
 
     @Test
     fun `test on add a new pet()`() {
+        assertTrue(petService.getAllPets().size == 0)
         petService.savePet(dogExample.toDTO())
+        assertTrue(petService.getAllPets().size == 1)
 
         val persistentDog = petService.getPetEntityById(1)
 
