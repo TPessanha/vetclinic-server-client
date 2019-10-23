0515a991ac20c0f81428a8049a1c1064c1680c4d
LuisMonteiro <lm.monteiro@campus.fct.unl.pt>
2019-10-23 01:29:38 +0100

    checkAppointments created

--- vetclinic-server/src/main/kotlin/personal/ciai/vetclinic/repository/ClientRepository.kt
+++ vetclinic-server/src/main/kotlin/personal/ciai/vetclinic/repository/ClientRepository.kt
@@ -1,13 +1,17 @@
 package personal.ciai.vetclinic.repository
 
 import java.util.Optional
+import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.jpa.repository.Query
-import org.springframework.data.repository.CrudRepository
 import org.springframework.stereotype.Repository
 import personal.ciai.vetclinic.model.Client
 
 @Repository
-interface ClientRepository : CrudRepository<Client, Int> {
+interface ClientRepository : JpaRepository<Client, Int> {
     @Query("select c from Client c left join fetch c.pets where c.id = :owner")
     fun findByIdWithPets(owner: Int): Optional<Client>
+
+    // A query that loads all Clients with prefetching of the appointments associated
+    @Query("select c from Client p left join fetch c.appointments where c.id = :id")
+    fun findByIdWithAppointment(id: Int): Optional<Client>
 }
