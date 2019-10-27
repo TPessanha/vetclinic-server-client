1cf05f9d9732267a3d5f569cc9b54b5ee623fd92
Tomas Pessanha <t.pessanha@campus.fct.unl.pt>
2019-10-27 22:52:25 +0000

    Cleanup 2

--- vetclinic-server/src/main/kotlin/personal/ciai/vetclinic/controller/PetController.kt
+++ vetclinic-server/src/main/kotlin/personal/ciai/vetclinic/controller/PetController.kt
@@ -5,7 +5,6 @@ import io.swagger.annotations.ApiOperation
 import io.swagger.annotations.ApiParam
 import io.swagger.annotations.ApiResponse
 import io.swagger.annotations.ApiResponses
-import java.security.Principal
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.MediaType
 import org.springframework.http.ResponseEntity
